df020e35d8fadf8d6d184e7576c8036c66924885
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2022-03-09 12:48:20 +0700

    dua activity dengan intent dan passing parameter

--- app/src/main/java/com/example/setklassa/MainActivity.java
+++ app/src/main/java/com/example/setklassa/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.setklassa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText edemail, edpassword;
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.btSignin);
+        edemail=findViewById(R.id.edEmail);
+        edpassword=findViewById(R.id.edPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validasi();
+            }
+        });
+    }
+
+    public void validasi(){
+        nama = edemail.getText().toString();
+        password = edpassword.getText().toString();
+        String email = "admin@mail.com";
+        String pass = "123";
+
+        if(nama.isEmpty() && password.isEmpty()){
+            edemail.setError("Email yang anda masukkan salah");
+            edpassword.setError("Password salah");
+            Toast.makeText(getApplicationContext(), "Email dan Password salah", Toast.LENGTH_SHORT).show();
+
+
+
+        }
+        if(nama.equals(email)&& password.equals(pass)){
+            Toast.makeText(getApplicationContext(), "Login Sukses", Toast.LENGTH_LONG).show();
+        }
+        Bundle b = new Bundle();
+        b.putString("a",nama.trim());
+        b.putString("b",password.trim());
+
+        Intent i = new Intent(getApplicationContext(),ActivityHasil.class);
+        i.putExtras(b);
+
+        startActivity(i);
+
     }
 }
